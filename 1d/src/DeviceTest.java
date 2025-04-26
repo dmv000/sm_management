d42955131fa565338125838e9d7b4301183b7c16
tino2006 <etienne.tabchoury@gmail.com>
2025-04-26 18:13:36 +0300

    fixed toString of Room and minor changes all around

--- 1d/src/DeviceTest.java
+++ 1d/src/DeviceTest.java
@@ -6,20 +6,27 @@ public class DeviceTest {
         A1.turnOn(1);
         System.out.println(A1.getCurrentConsumption());
         System.out.println(A1);
-        A1.turnOff();
+        //A1.turnOff();
         System.out.println(A1);
 
-        Light L1 = new Light(100, "Light", 50, false, true);
+        Light L1 = new Light(150, "Light", 50, false, true);
         L1.turnOn(50);
         System.out.println(L1.getCurrentConsumption());
         System.out.println(L1);
-        L1.turnOff();
+        //L1.turnOff();
         System.out.println(L1);
 
         Room r1= new Room("L1", "Living Room");
         r1.addDevice(L1);
         r1.addDevice(A1);
+        r1.removeDevice(A1);
         System.out.println(r1);
+        System.out.println(r1.getCurrentConsumption());
 
+        System.out.println(r1.searchDeviceById(100));
+        ManagementSystem M1 = new ManagementSystem("123456Ab","123456Ba");
+        M1.addRoom(r1);
+        System.out.println(M1.displayInfo());
+        System.out.println(M1.displayDetailsOneRoom("L1"));
     }
 }
